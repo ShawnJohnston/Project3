40ed2492d90a21367ead92cb6040b356387bdb2d
ShawnJohnston <44795446+ShawnJohnston@users.noreply.github.com>
2023-04-18 15:18:28 -0400

    .

--- Project3/Graph.h
+++ Project3/Graph.h
@@ -1,25 +1,25 @@
 #pragma once
 
 #include <string>
+#include <map>
+#include <vector>
 
 using namespace std;
 
-class Node {
+class Cat {
 	// order,id,url,type,age,gender,size,coat,breed,photos,med_photos
 
-	int order;
-	int id;
-	string age;
-	string gender;
-	string size;
-	string coat;
-	string breed;
-	string photo;
+	int order = -1;
+	int id = -1;
+	string age = "";
+	string gender = "";
+	string size = "";
+	string coat = "";
+	string breed = "";
+	string photo = "";
 
-	Node() {
-
-	}
-	Node(int& order, int& id, string& age, string& gender, string& size, string& coat, string& breed, string& photo) {
+	Cat() {}
+	Cat(int& order, int& id, string& age, string& gender, string& size, string& coat, string& breed, string& photo) {
 		this->order = order;
 		this->id = id;
 		this->age = age;
@@ -29,4 +29,28 @@ class Node {
 		this->breed = breed;
 		this->photo = photo;
 	}
+};
+
+class Category {
+	string category;
+	vector<Cat*> cats;
+
+public:
+	Category() {};
+	Category(string& category) {
+		this->category = category;
+	}
+	void insertCat(Cat*& cat) {
+		cats.push_back(cat);
+	}
+};
+
+
+class Graph {
+	map<string, Category*> categories;
+	int size = 0;
+
+	public:
+		Graph() {};
+
 };
---
+++ Project3/Menu.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Graph.h"
+
+class Menu {
+	void compileGraph();
+public:
+	Menu();
+
+	void MainMenu();
+	void SearchMenu();
+	void DataMenu();
+	void CompletionMenu();
+};
